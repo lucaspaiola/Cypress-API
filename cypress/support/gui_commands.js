2339d4ff569a6ebd237382df06c8b745aec59c14
lucaspaiola <lucas.v.paiola@gmail.com>
2024-07-02 17:50:31 -0300

    Adicionando testes para SetMilestoneOnIssue

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -64,3 +64,7 @@ Cypress.Commands.add('gui_setLabelOnIssue', label => {
   cy.get('body').click()
 })
   
+Cypress.Commands.add('gui_setMilestoneOnIssue', milestone => {
+  cy.get('.block.milestone .edit-link').click()
+  cy.contains(milestone.title).click()
+})
